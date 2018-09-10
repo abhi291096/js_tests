d27f87ccf39c52c23d37f1a90b502e6edf97676f
abhishek mordani <abhishekmordani@abhisheks-MacBook-Pro.local>
2018-09-10 17:26:55 +0530

    Done 2nd Test

---
+++ test2.js
@@ -0,0 +1,50 @@
+var webdriver = require('selenium-webdriver');
+var assert = require('assert');
+
+
+
+//By = webdriver.By,
+  //  until = webdriver.until;
+
+
+
+var capabilities = {
+    'browserName': 'chrome'
+}
+
+
+var driver = new webdriver.Builder().usingServer('http://10.100.100.228:4444/wd/hub').withCapabilities(capabilities).build();
+
+driver.get('https://the-internet.herokuapp.com/javascript_alerts');
+
+
+
+
+
+driver.findElement({ xpath: '//*[@id="content"]/div/ul/li[1]/button' }).click();
+
+driver.switchTo().alert().accept();
+driver.takeScreenshot("proof.png");
+
+
+ driver.findElement({ xpath: '//*[@id="content"]/div/ul/li[2]/button' }).click();
+
+ driver.switchTo().alert().accept();
+
+driver.findElement({ xpath: '//*[@id="content"]/div/ul/li[3]/button' }).click();
+
+driver.switchTo().alert().sendKeys("Abhishek");
+driver.switchTo().alert().accept();
+
+var result = driver.findElement({id : 'result'}).getText();
+
+assert.equal("You entered: Abhishek",result.value_);
+
+
+// Click 'Click for JS Alert'.
+
+// Click 'Click for JS Confirm'. Clicks 'OK'. Assert the Result value 'You clicked: Ok'
+
+// Click 'Click for JS Prompt'. Enter 'BrowserStack' in the prompt. Assert the Result value 'You entered: BrowserStack' Screen reader support enabled.
+
+driver.quit();

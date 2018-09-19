aa0f730d421abc214fabdc9c5a728bd44f8e72cf
abhishek mordani <abhishekmordani@abhisheks-MacBook-Pro.local>
2018-09-19 11:36:57 +0530

    rest api command in node js to check plan

---
+++ samp1.js
@@ -0,0 +1,49 @@
+var https = require('https')
+
+var username = 'abhishekmordani2'
+var passw = 'yNdm9XxyX7esbsk4Uqg6'
+
+var options = {
+host: 'api.browserstack.com',
+port: 443,
+path: '/automate/plan.json',
+// authentication headers
+headers: {
+'Authorization': 'Basic ' + new Buffer(username + ':' + passw).toString('base64')
+}
+};
+
+//this is the call
+request = https.get(options, function (res) {
+var body = "";
+res.on('data', function (data) {
+body += data;
+});
+res.on('end', function () {
+//here we have the full response, html or json object
+console.log(body);
+})
+res.on('error', function (e) {
+onsole.log("Got error: " + e.message);
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
